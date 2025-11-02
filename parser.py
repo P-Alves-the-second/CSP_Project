59597789f352a195dcdbf4255fadf6780ff9a593
Pedro Alves <a27986@alunos.ipca.pt>
2025-11-02 12:51:54 +0000

    removing some comments

--- parser.py
+++ parser.py
@@ -12,20 +12,16 @@ def parse_dataset(dataset_text: str):
         if not line or line.startswith("#head"):
             continue
         
-        # Detecta nova seção (ex: #cc, #dsd, #tr, etc.)
         if line.startswith("#"):
             current_section = line[1:].split("—")[0].strip()
             sections[current_section] = []
             continue
-        
-        # Adiciona linha à seção atual
+
         if current_section:
             sections[current_section].append(line)
 
-    # Converte seções em estruturas Python
     parsed = {}
 
-    # #cc — courses assigned to classes (class, courses*)
     if "cc" in sections:
         class_courses = {}
         for l in sections["cc"]:
@@ -33,11 +29,9 @@ def parse_dataset(dataset_text: str):
             class_courses[parts[0]] = parts[1:]
         parsed["class_courses"] = class_courses
 
-    # #olw — courses with just one lesson per week
     if "olw" in sections:
         parsed["one_lesson_week"] = [l.strip() for l in sections["olw"]]
 
-    # #dsd — courses assigned to lecturers (teacher, courses*)
     if "dsd" in sections:
         teacher_courses = {}
         for l in sections["dsd"]:
@@ -45,7 +39,6 @@ def parse_dataset(dataset_text: str):
             teacher_courses[parts[0]] = parts[1:]
         parsed["teacher_courses"] = teacher_courses
 
-    # #tr — timeslot restrictions (teacher, slots_unavailable*)
     if "tr" in sections:
         teacher_restrictions = {}
         for l in sections["tr"]:
@@ -53,7 +46,6 @@ def parse_dataset(dataset_text: str):
             teacher_restrictions[parts[0]] = list(map(int, parts[1:]))
         parsed["teacher_restrictions"] = teacher_restrictions
 
-    # #rr — room restrictions (course, room)
     if "rr" in sections:
         room_restrictions = {}
         for l in sections["rr"]:
@@ -61,7 +53,6 @@ def parse_dataset(dataset_text: str):
             room_restrictions[parts[0]] = parts[1]
         parsed["room_restrictions"] = room_restrictions
 
-    # #oc — online classes (course, lesson_week_index)
     if "oc" in sections:
         online_classes = {}
         for l in sections["oc"]:
